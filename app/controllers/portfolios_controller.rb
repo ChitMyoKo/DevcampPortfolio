8fb52502f77cca4fc291a4c55968d92261102d1c
ChitMyoKo <chitmyoko.2001@gmail.com>
2022-03-11 18:44:39 +0630

    Integrated new and actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,22 @@ class PortfoliosController < ApplicationController
     def index
         @portfolios = Portfolio.all
     end
+
+    def new
+        @portfolio = Portfolio.new
+    end
+
+    def create
+        @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio.save
+            format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
+            format.json { render :show, status: :created, location: @portfolio }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 end

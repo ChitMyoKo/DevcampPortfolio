44860779b87a02daa028310ac0126341c390c9e8
ChitMyoKo <chitmyoko.2001@gmail.com>
2022-03-15 15:11:04 +0630

    Implemented Concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :percent_utilized
+
+    after_initialize :set_defaults
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height: '350', width:'150')
+    end
 end
